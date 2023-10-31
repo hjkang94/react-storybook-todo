eb775e421efbe81d3a07b4a268de472cc3a26691
hjkang94 <hjkang94@github.com>
2023-10-31 13:43:25 +0900

    App.tsx 수정, 라우터 추가

--- src/components/templates/ToDoList/index.tsx
+++ src/components/templates/ToDoList/index.tsx
@@ -8,7 +8,7 @@ const Container = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   background-color: #eeeeee;
 `;
